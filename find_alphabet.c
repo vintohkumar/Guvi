129bf176955023f097768def8f5ca5e3cf25510e
vintohkumar <vinothkumar0896@gmail.com>
2018-04-01 09:39:55 +0530

    Update find_alphabet.c

--- find_alphabet.c
+++ find_alphabet.c
@@ -1,8 +1,9 @@
-#inlcude<stdio.h>
+#include<stdio.h>
+
 int main(){
   char letter;
-  scanf("%c",letter);
-  (c>='a'||c<='z'||c>='A'||c<='Z')?printf("Alphabet"):printf("NO");
-  
-  return 0; 
+  scanf("%c",&letter);
+  (letter>='a'&&letter<='z'||letter>='A'&&letter<='Z')?printf("Alphabet"):printf("NO");
+  return 0;
 }
+
